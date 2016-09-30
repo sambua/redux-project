ea12c47fffc755628ba21898e9a8260be12ef9a0
Rashad Aliyev <aliyev.resad@gmail.com>
2016-09-30 22:45:59 +0400

    Process before implementing API approach

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,69 @@
+import React, {PropTypes} from 'react';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
+
+class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title: "" }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
+  onClickSave() {
+    this.props.actions.createCourse(this.state.course);
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
+        <h2>Add Cource</h2>
+        <input type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+
+        <input type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
+      </div>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  //dispatch: PropTypes.func.isRequired,  // No require when call above dispacth
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return{
+    courses: state.courses  // it's reducer function name which is called in combineReducers
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    // createCourse: course => dispatch(courseActions.createCourse(course)) // we use above when adding bindActionCreators
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
